39ba7b2f73138e15e5667f9343a1f24fe71b9ac3
fscarv <fernando.carvalho@lcsistemas.com.br>
2024-10-11 15:49:56 -0300

    Service layer, component registration

--- src/main/java/com/scarv/course/controller/UserController.java
+++ src/main/java/com/scarv/course/controller/UserController.java
@@ -1,18 +1,34 @@
 package com.scarv.course.controller;
 
 import com.scarv.course.entity.User;
+import com.scarv.course.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "Fernando", "qj5wz@example.com", "999999999", "123456");
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        return userService.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 }
---
+++ src/main/java/com/scarv/course/service/UserService.java
@@ -0,0 +1,26 @@
+package com.scarv.course.service;
+
+import com.scarv.course.entity.User;
+import com.scarv.course.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findById(Long id){
+        return userRepository.findById(id);
+    }
+}
